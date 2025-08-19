644f8447b787492139b8c5b4cf26677cfd95eb19
SattvaProducer <satvaproducer@gmail.com>
2025-08-19 19:10:41 +0300

    Initial commit

---
+++ test_env.py
@@ -0,0 +1,17 @@
+﻿from dotenv import load_dotenv
+import os
+
+# Загружаем переменные окружения
+load_dotenv()
+
+# Проверяем каждую переменную отдельно
+api_key = os.getenv('DEEPSEEK_API_KEY')
+app_secret = os.getenv('APP_SECRET')
+
+print('API ключ:', repr(api_key))  # repr покажет точное значение
+print('Секрет:', repr(app_secret))
+
+# Альтернативный способ проверки
+print('\nПроверка через os.environ:')
+print('API ключ:', repr(os.environ.get('DEEPSEEK_API_KEY')))
+print('Секрет:', repr(os.environ.get('APP_SECRET')))
